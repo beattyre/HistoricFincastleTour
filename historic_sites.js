8c51d619238bed4e484e26b5dc71989eca2199df
beattyre <beattyre@miamioh.edu>
2016-03-08 15:15:30 -0500

    Update historic_sites.js

--- historic_sites.js
+++ historic_sites.js
@@ -1,12 +1,12 @@
 	// Create map variable
-var map = L.map('map').setView([37.499, -79.874], 18);
+var map = L.map('map').setView([37.498515, -79.876458], 17);
 
 // Add basemap from Mapbox
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken} ', {
   attribution: 'Basemap from <a href="http://mapbox.com">Mapbox</a>,   ' +
   'design by <a href="http://co.botetourt.va.gov>Botetourt County</a>. ',
   maxZoom: 20,
-  minZoom: 17,
+  minZoom: 15,
   id: 'beattyre.pbhimakk',
   accessToken: 'pk.eyJ1IjoiYmVhdHR5cmUiLCJhIjoiMjFmNWViOGFhMmJmZjZjOGZlODU3MDM1MzNlZmM5N2UifQ.OTWiU0TYOnTvH6H6DSngTg'
 	}).addTo(map);
