1eaaae0ecd3bb3a20a51db8c16882926d24c3b21
beattyre <beattyre@miamioh.edu>
2016-03-08 14:41:22 -0500

    Update historic_sites.js

--- historic_sites.js
+++ historic_sites.js
@@ -13,7 +13,7 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 // Start GeoJSON
 function addDataToMap(data, map) {
 	var dataLayer = L.geoJSON(data, {
-		onEachFeature: function(function, layer) {
+		onEachFeature: function(feature, layer) {
 			var popupText = "Stop Number: " + feature.properties.Stop_No
 			+ "<br>Location Name: " +feature.properties.name;
 			layer.bindPopup(popupText); }
