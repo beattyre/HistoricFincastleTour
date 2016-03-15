8b1aee0530cb86dde95a0a1017f29c0729d6bbb0
beattyre <beattyre@miamioh.edu>
2016-03-15 13:17:31 -0400

    Update historic_sites.js

--- historic_sites.js
+++ historic_sites.js
@@ -11,54 +11,16 @@ function initmap() {
     attribution: osmAttributes
   });
 
-  map.setView(new L.LatLng(37.499, -79.875), 15);
+  map.setView(new L.LatLng(37.499, -79.875), 17);
   map.addLayer(osm);
 };
 initmap();
 
-
-$.getJSON("https://rawgit.com/beattyre/WebMapTest/gh-pages/TestPoints.geojson", function(sitesData) {
-  L.geoJson(sitesData).addTo(map);
+$.getJSON("https://rawgit.com/beattyre/WebMapTest/gh-pages/TestPoints.geojson", function(data) {
+  L.geoJson(data, {
+  onEachFeature: function(feature, layer){
+    layer.bindPopup("<b>Stop Number: </b>" + feature.properties.Stop_No +
+        "<br><b>Location Name: </b>" + feature.properties.name)
+  }
+  }).addTo(map);
 });
-
-function highlightFeature(e){
-  var layer = e.target;
-  layer.setStyle({
-    color: '#000',
-  });
-  info.update(layer.feature.properties);
-}
-
-function resetHighlight(e){
-  geojson.resetStyle(e.target);
-  info.update();
-}
-
-var geojson;
-
-function onEachFeature(feature, layer) {
-    layer.on({
-        click: highlightFeature,
-    });
-}
-
-geojson = L.geoJson(sitesData, {
-    style: style,
-    onEachFeature: onEachFeature
-}).addTo(map);
-
-
-var info = L.control();
-
-info.onAdd = function(map) {
-  this._div = L.DomUtil.create('div', 'info');
-  this.update();
-  return this._div;
-};
-
-info.update = function(props){
-  this._div.innterHTML = "<b>Stop Number: </b>" + (props.Stop_No +
-        "<br><b>Location Name: </b>" + props.name);
-};
-
-info.addTo(map);
