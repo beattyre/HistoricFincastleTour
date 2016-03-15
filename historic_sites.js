2ba82466535850980c23829c103643ab40e6ea28
beattyre <beattyre@miamioh.edu>
2016-03-15 09:04:28 -0400

    Update historic_sites.js

--- historic_sites.js
+++ historic_sites.js
@@ -1,24 +1,35 @@
-	// Create map variable
-var map = L.map('map').setView([37.498515, -79.876458], 17);
+var map;
 
-// Add basemap from Mapbox
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken} ', {
-  attribution: 'Basemap from <a href="http://mapbox.com">Mapbox</a>,   ' +
-  'design by <a href="http://co.botetourt.va.gov>Botetourt County</a>. ',
-  maxZoom: 20,
-  minZoom: 15,
-  id: 'beattyre.pbhimakk',
-  accessToken: 'pk.eyJ1IjoiYmVhdHR5cmUiLCJhIjoiMjFmNWViOGFhMmJmZjZjOGZlODU3MDM1MzNlZmM5N2UifQ.OTWiU0TYOnTvH6H6DSngTg'
-	}).addTo(map);
+function initmap() {
+  map = new L.map('map');
+  // Add basemap from OSM
+  var osmHot = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+  var osmAttributes = 'Map Data @ <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, Tiles courtesy of <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OpenStreetMap Team</a>';
+  var osm = new L.tileLayer(osmHot, {
+    minZoom: 12,
+    maxZoom: 20,
+    attribution: osmAttributes
+  });
+
+  map.setView(new L.LatLng(37.499, -79.875), 17);
+  map.addLayer(osm);
+};
+initmap();
+
+
+
+$.getJSON("https://rawgit.com/beattyre/WebMapTest/gh-pages/TestPoints.geojson", function(data) {
+  L.geoJson(data).addTo(map);
+});
+
 // Start GeoJSON
 function addDataToMap(data, map) {
-	var dataLayer = L.geoJson(data, {
-		onEachFeature: function(feature, layer) {
-			var popupText = "<b>Stop Number: </b>" + feature.properties.Stop_No
-			+ "<br><b>Location Name: </b>" +feature.properties.name;
-			layer.bindPopup(popupText); }
-		});
-	dataLayer.addTo(map);
-}
-
-$.getJSON("https://rawgit.com/beattyre/WebMapTest/gh-pages/TestPoints.geojson", function(data) { addDataToMap(data,map); });
+  var dataLayer = L.geoJson(data, {
+    onEachFeature: function(feature, layer) {
+      var popupText = "<b>Stop Number: </b>" + feature.properties.Stop_No +
+        "<br><b>Location Name: </b>" + feature.properties.name;
+      layer.bindPopup(popupText);
+    }
+  });
+  dataLayer.addTo(map);
+};
